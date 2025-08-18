f0df3341be50bf423b83cfe839d0c37e8d9b8ab1
terence planchestainer <t.planchestainer@stag.technofuturtic.education>
2025-08-18 18:52:53 +0200

    Effectuez des opérations CRUD dans l'administration de Django

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from listings.models import Band
+from listings.models import Listing
+
+admin.site.register(Band)
+admin.site.register(Listing)
